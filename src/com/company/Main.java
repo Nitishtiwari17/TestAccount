0011cb74ea3c10962a73f02a53547983a0ca6295
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2021-11-25 00:16:45 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+class Account
+{
+    int acc_no;
+    String name;
+    float amount;
+    void insert(int a,String n,float amt)
+    {
+        acc_no=a;
+        name=n;
+        amount=amt;
+    }
+    void deposit(float amt){
+        amount=amount+amt;
+        System.out.println(amt+"deposit");
+    }
+    void withdraw(float amt){
+        if(amount<amt){
+            System.out.println("insufficient Balance");
+        }
+        else{
+            amount=amount-amt;
+            System.out.println(amt+"withdraw");
+        }
+    }
+    void checkBalance(){
+        System.out.println("Balance is:"+amount);
+    }
+    void display(){
+        System.out.println(acc_no+" "+name+" "+amount);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        Account a1=new Account();
+        a1.insert(832345,"nitish",1000);
+        a1.display();
+        a1.checkBalance();
+        a1.deposit(40000);
+        a1.checkBalance();
+        a1.withdraw(15000);
+        a1.checkBalance();
+    }
+	// write your code here
+    }
+
+
